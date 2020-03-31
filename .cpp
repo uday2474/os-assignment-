d1c1174ee0335f05e10c792522490880ae50ec06
uday <62943337+uday2474@users.noreply.github.com>
2020-03-31 19:36:07 +0530

    Update and rename .gitignore to .cpp

---
+++ .cpp
@@ -0,0 +1,18 @@
+Include<stdio.h>
+Include<conio.h>
+{
+Void main()
+{
+Sem ear-avail=0, car-taken=0, car-filled=0, Passenger- released=0;
+
+Process passenger[i=1 to m]
+{
+While(true)do {
+Sleep (random (1000*museo-time)); P(car-avail); v(car-taken); p(car-filled); P(passenger-released);
+}
+}
+Process car[i=1 to n]
+{
+While (true) {
+V (car-avail); p(car-taken); v (car-filled); Sleep (random(1000*ride-time); V(passenger – released);
+}
